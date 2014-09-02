b2e50f9ebf862673fdc5e4c10962bb0eb6f3d97f
adisleydis <arodrig@thoughtworks.com>
2014-09-02 10:05:03 -0500

    [#0] Se renombro el user journey de administrar usuarios y se creo la prueba del registro civil

---
+++ AdministrarUsuario.scala
@@ -0,0 +1,86 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import io.gatling.http.response._
+
+
+  class loginPage extends Simulation { 
+
+  val httpConf = http
+    .baseURL("https://181.112.147.247:8443/")
+    .acceptHeader("*/*")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-us,en;q=0.5")
+    .userAgentHeader("foo")
+    .connection("keep-alive")
+    .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36")
+
+  val login = scenario("Administrar usuarios")
+    .exec(http("Login")
+      .post("identificacion")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .body(StringBody(
+        """{
+              "nombreUsuario": "admin",
+              "contrasenia": "Sni3s3Adm1nTW"
+        }""")).asJSON
+      .check(status.is(201), bodyString.saveAs("bearer"))
+    )
+    .pause(4)
+
+    .exec(http("Crear perfil")
+      .post("perfiles")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .header("Authorization", "Bearer ${bearer}")
+      .body(StringBody(
+        """{
+              "nombre":"administrador",
+              "permisos":[{"id":9}]
+
+        }""")).asJSON
+            .check(status.is(201))
+    )
+    .pause(4)
+
+    .exec(http("Crear usuario")
+      .post("usuario")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .header("Authorization", "Bearer ${bearer}")
+      .body(StringBody(
+        """{
+              "identificacion":{"tipoDocumento":"pasaporte","numeroIdentificacion":"AGAH"},
+              "nombre":{"primerNombre":"ana","primerApellido":"ana"},
+              "institucion":{"id":"2005"},"perfiles":[1],
+              "emailInstitucional":"seneteam@gmail.com",
+              "numeroAutorizacionQuipux":"SENESCYT-AB-2014-12345-MI",
+              "finDeVigencia":"2014-10-30",
+              "nombreUsuario":"lala","estado":"ACTIVO"
+        }""")).asJSON
+            .check(status.is(201))
+    )
+    .pause(4)
+    
+     .exec(http("Listar usuarios")
+      .get("usuario/todos")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .header("Authorization", "Bearer ${bearer}")
+      .check(status.is(200))
+    )
+    .pause(4)
+
+    .exec(http("paises")
+      .get("paises")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .header("Authorization", "Bearer ${bearer}")
+      .check(status.is(200))
+    )
+    .pause(4)
+    
+  setUp(
+    login.inject(
+      atOnceUsers(500))
+      .protocols(httpConf)
+  )
+}
--- RegistroCivil.scala
+++ RegistroCivil.scala
@@ -6,10 +6,10 @@ import scala.concurrent.duration._
 import io.gatling.http.response._
 
 
-  class loginPage extends Simulation { 
+  class registroCivil extends Simulation { 
 
   val httpConf = http
-    .baseURL("https://10.71.20.26:8443/")
+    .baseURL("https://181.112.147.247:8443/")
     .acceptHeader("*/*")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-us,en;q=0.5")
@@ -17,29 +17,27 @@ import io.gatling.http.response._
     .connection("keep-alive")
     .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36")
 
-  val login = scenario("Login")
-    .exec(http("Login")
+   val login = scenario("Validar cedula")
+    .exec(http("Identificacion")
       .post("identificacion")
       .header("Content-Type", "application/json; charset=UTF-8")
       .body(StringBody(
         """{
-                      "nombreUsuario": "admin",
-                      "contrasenia": "Sni3s3Adm1nTW"
+              "nombreUsuario": "admin",
+              "contrasenia": "Sni3s3Adm1nTW"
         }""")).asJSON
       .check(status.is(201), bodyString.saveAs("bearer"))
     )
-    .pause(2 seconds)
-    
-    .exec(http("paises")
-      .get("paises")
-      .header("Content-Type", "application/json; charset=UTF-8")
-      .header("Authorization", "Bearer ${bearer}")
-      .check(status.is(200))
-    )
-    
+   .pause(4)
 
-  setUp(
-    login.inject(
-      rampUsersPerSec(50) to(60) during(10 seconds)).protocols(httpConf)
-  )
-}
+    .exec(http("Registro civil")
+      .post("busqueda?cedula=1111111116")
+      .header("Content-Type", "application/json; charset=UTF-8")
+      .check(status.is(401))
+    )
+    .pause(4)
+    
+    setUp(
+    login.inject(atOnceUsers(10)).protocols(httpConf)
+    )
+  }
