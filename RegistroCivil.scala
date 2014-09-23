66b623c475f47f7935e63165e9761f7f8a8e24f1
adisleydis <arodrig@thoughtworks.com>
2014-09-23 11:20:17 -0500

    [#0] con @jreyes33 Se actualizo el fichero RegistroCivil

--- RegistroCivil.scala
+++ RegistroCivil.scala
@@ -6,10 +6,14 @@ import scala.concurrent.duration._
 import io.gatling.http.response._
 
 
-  class registroCivil extends Simulation {
+object RegistroCivil {
+  var token = ""
+}
 
+class RegistroCivil extends Simulation {
+
+  val urlBase = "https://10.0.9.212"
   val httpConf = http
-    .baseURL("https://10.0.9.212:8443/")
     .acceptHeader("*/*")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-us,en;q=0.5")
@@ -17,33 +21,43 @@ import io.gatling.http.response._
     .connection("keep-alive")
     .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36")
 
-   val credenciales = csv("credenciales.csv").random
-   val cedulas = csv("cedulas.csv").random
-   val login = scenario("Validar cedula")
+  val credenciales = csv("credenciales.csv").random
+  val login = scenario("Login")
     .feed(credenciales)
     .exec(http("Identificacion")
-      .post("identificacion")
-      .header("Content-Type", "application/json; charset=UTF-8")
+      .post(urlBase + ":8447/autenticacion")
+      .header("Content-Type", "application/json;charset=UTF-8")
       .body(StringBody(
         """{
               "nombreUsuario":"${usuario}",
               "contrasenia":"${contrasenia}"
         }""")).asJSON
-      .check(status.is(201), bodyString.saveAs("bearer"))
+      .check(jsonPath("$.token").saveAs("tokenUnico"))
+      .check(status.is(201))
     )
-   .pause(4)
-
-    .feed(cedulas)
-    .exec(http("Registro civil")
-      .get("busqueda?cedula=${cedula}")
-      .header("Content-Type", "application/json; charset=UTF-8")
-      .header("Authorization", "Bearer ${bearer}")
-      .check(status.is(200))
-    )
+    .exec((session: Session) => {
+      RegistroCivil.token = session("tokenUnico").as[String]
+      session
+    })
     .pause(4)
 
-    setUp(
-    login.inject(rampUsersPerSec(1) to(5) during(60 minutes))
-    .protocols(httpConf)
+  val cedulas = csv("cedulas.csv").random
+  val validarCedula = scenario("Validar cedula")
+    .feed(cedulas)
+    .exec((session: Session) => session.set("tokenUnico", RegistroCivil.token))
+    .exec(http("Registro civil")
+      .get(urlBase + ":8443/busqueda?cedula=${cedula}")
+      .header("Content-Type", "application/json;charset=UTF-8")
+      .header("Authorization", "${tokenUnico}")
+      .check(status.is(200))
+    ).pause(4)
+
+
+  setUp(
+    login.inject(atOnceUsers(1)),
+    validarCedula.inject(
+      nothingFor(10),
+      rampUsersPerSec(1) to(5) during(60 minutes)
     )
-  }
+  ).protocols(httpConf)
+}
