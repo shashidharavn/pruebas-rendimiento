a46074f2c0bd32cc2893da3a67fe4020fd0ca2f5
Fausto Castaneda <fausto.castaneda.v@gmail.com>
2014-10-20 15:53:55 -0500

    [#564] con @nelsongonzalez - Tiene script de limpiar base de datos

---
+++ data/scripts/drop_schemas_sniese.sql
@@ -0,0 +1,7 @@
+﻿DO $$DECLARE sentencia_truncate RECORD;
+generar_sentencias_truncate CURSOR FOR SELECT 'DROP SCHEMA ' || schema_name || ' CASCADE;' script FROM information_schema.schemata WHERE schema_name LIKE 'servicio%';
+BEGIN
+FOR sentencia_truncate in generar_sentencias_truncate LOOP
+  execute sentencia_truncate.script;
+END LOOP;
+END$$;
