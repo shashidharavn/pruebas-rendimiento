2795c1b1143b33cb93792d8393b4ac27a06f1c2c
Fausto Castaneda <fausto.castaneda.v@gmail.com>
2014-10-28 16:04:13 -0500

    [#564] con @nelsongonzalez - Se agregan variables de entorno para las credenciales de QA

--- data/scripts/reiniciar_microservicios.sh
+++ data/scripts/reiniciar_microservicios.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/expect -f
-spawn ssh thoughtworks@181.112.147.247
+spawn ssh $env(QA_USER)@$env(QA_HOST)
 expect -re "assword: "
-send "official north beauty game\r"
+send "$env(QA_PASSWORD)\r"
 send "sudo su -\r"
 send "cd /etc/init.d\r"
 send "for i in \$(ls -d servicio-*)\r"
 send "do\r"
 send "echo \"Reiniciando \$i...\"\r"
-# send "service \$i restart\r"
+send "service \$i restart\r"
 send "done\r"
 send "exit\r"
 send "exit\r"
+interact
