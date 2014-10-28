3b4e208684ef9bd153a71be429e3f5493b623166
Fausto Castaneda <fausto.castaneda.v@gmail.com>
2014-10-28 15:58:13 -0500

    [#564] con @nelsongonzalez - Se agrega script para reinicio de microservicios

--- data/scripts/reiniciar_microservicios.sh
+++ data/scripts/reiniciar_microservicios.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-ssh thoughtworks@181.112.147.247
-expect "assword:"
-send "official north beauty game"
-interact
-sudo su -
-cd /etc/init.d
-microservicios = ($(ls -d servicio*/))
-for i in "${microservicios[@]}"
-do
-  #service $i restart
-  echo $i
-done
+#!/usr/bin/expect -f
+spawn ssh thoughtworks@181.112.147.247
+expect -re "assword: "
+send "official north beauty game\r"
+send "sudo su -\r"
+send "cd /etc/init.d\r"
+send "for i in \$(ls -d servicio-*)\r"
+send "do\r"
+send "echo \"Reiniciando \$i...\"\r"
+# send "service \$i restart\r"
+send "done\r"
+send "exit\r"
+send "exit\r"
