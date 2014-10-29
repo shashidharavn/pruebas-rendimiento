a0282f6bc0d4335efa4c4721609f1beb58568df2
Fausto Castaneda <fausto.castaneda.v@gmail.com>
2014-10-29 14:50:43 -0500

    [#564] - Se agregan variables de entorno para correr el reinicio de los microservicios

--- data/scripts/iniciar_restart.sh
+++ data/scripts/iniciar_restart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sshpass -p 'official north beauty game' ssh -oStrictHostKeyChecking=no -tt thoughtworks@10.0.9.212 'bash -s' < restart_microservices.sh
+sshpass -e ssh -oStrictHostKeyChecking=no -tt $QA_USER@$QA_HOST 'bash -s' < restart_microservices.sh
